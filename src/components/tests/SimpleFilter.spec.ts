acba7c1aac4705f6593da044376f62aea6f8652a
xHugo21 <hugog@hugog.empathy.ts.net>
2023-04-14 10:38:51 +0200

    Removed tests files due to errors on deployment

--- src/components/tests/SimpleFilter.spec.ts
+++
@@ -1,8 +0,0 @@
-import SimpleFilter from '../SimpleFilter.vue'
-import { mount } from '@vue/test-utils'
-import { test, expect, describe } from 'vitest'
-
-describe('SimpleFilter', () => {
-  test('renders the filter label', () => {
-    // Our first test
-  })
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
@@ -12,8 +12,5 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
-  test: {
-    environment: 'jsdom'
-  },
+  }
 })
